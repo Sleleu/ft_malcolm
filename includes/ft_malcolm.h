c017235f851b8ccbfa8ca33e556b7e65505b7240
sleleu <leleu.sebastien@hotmail.fr>
2024-04-12 20:08:28 +0200

    add arp header

--- includes/ft_malcolm.h
+++ includes/ft_malcolm.h
@@ -2,4 +2,45 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <signal.h>
+#include <netinet/ip.h>
+#include "../libft/libft.h" 
+
+#define IP_SRC 1
+#define MAC_SRC 2
+#define IP_TARGET 3
+#define MAC_TARGET 4
+
+#define ARP_TYPE 0x0806
+
+#define ETHERNET_10MB 0x0001
+#define IPV4_TYPE 0x0800
+#define ETHERNET_ADDR_LEN 6
+#define IP_ADDR_LEN 4
+#define REQUEST 0x0001
+#define REPLY 0x0002
+
+typedef struct s_arp_hdr {
+    u_int16_t hardware_type;
+    u_int16_t proto_type;
+    u_int8_t hardware_addr_len;
+    u_int8_t proto_addr_len;
+    u_int16_t operation; // request or reply
+    char mac_src[ETHERNET_ADDR_LEN];
+    char ip_src[IP_ADDR_LEN];
+    char mac_target[ETHERNET_ADDR_LEN];
+    char ip_target[IP_ADDR_LEN];
+} t_arp_hdr;
+
+
+typedef struct s_data
+{
+	int					sockfd;
+	struct addrinfo		*result;
+	struct sockaddr_in	*sockaddr;
+}	t_data;
+
+extern t_data g_data;
--- srcs/main.c
+++ srcs/main.c
@@ -1,16 +1,83 @@
 #include "../includes/ft_malcolm.h"
 
+t_data g_data;
+
 void exit_error(char *message) {
     fprintf(stderr, "%s\n", message);
     exit(EXIT_FAILURE);
 }
 
+void signal_handler(int signal) {
+    if (signal == SIGINT) {
+        printf("Exit with signal\n");
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void init_sockaddr(char *ip_target)
+{
+	struct addrinfo 	hints;
+	struct addrinfo		*result;
+
+	ft_memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_RAW;
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_protocol = IPPROTO_ICMP;
+	int status = getaddrinfo(ip_target, NULL, &hints, &result);
+    if (status != 0) {
+        fprintf(stderr, "ft_malcolm: unknow host or invalid IP address: (%s).\n", ip_target);
+        exit(EXIT_FAILURE);
+    }
+    g_data.result = result;	
+    g_data.sockaddr = (struct sockaddr_in *)result->ai_addr;
+}
+
+
+void print_debug(void) {
+    printf("g_data.sockaddr->sin_addr.s_addr: %d\n", g_data.sockaddr->sin_addr.s_addr);
+
+    // ARP PACKET
+}
+
+t_arp_hdr create_arp_packet(char **argv) {
+    t_arp_hdr packet;
+    packet.hardware_type = ETHERNET_10MB;
+    packet.proto_type = IPV4_TYPE;
+    packet.hardware_addr_len = ETHERNET_ADDR_LEN;
+    packet.proto_addr_len = IP_ADDR_LEN;
+    packet.operation = REPLY;
+    ft_memcpy(packet.mac_src, argv[MAC_SRC], MAC_SRC);
+    ft_memcpy(packet.ip_src, argv[IP_SRC], IP_SRC);
+    ft_memcpy(packet.mac_target, argv[MAC_TARGET], MAC_TARGET);
+    ft_memcpy(packet.ip_target, argv[IP_TARGET], IP_TARGET);
+    return (packet);
+}
+
+void print_packet(t_arp_hdr packet) {
+    printf("----- ARP HEADER -----\n");
+    printf("TOTAL size: 28 bytes\n");
+    printf("hardware type: %d\n", packet.hardware_type);
+    printf("protocol type: %d (IPV4)\n", packet.proto_type);
+    printf("hardware address len: %d\n", packet.hardware_addr_len);
+    printf("protocole address len: %d\n", packet.proto_addr_len);
+    printf("operation: %d\n", packet.operation);
+    printf("sender hardware address: %s\n", packet.mac_src);
+    printf("sender protocol address: %s\n", packet.ip_src);
+    printf("target hardware address: %s\n", packet.mac_target);
+    printf("target protocol address: %s\n", packet.ip_target);
+}
 
 int main(int argc, char **argv) {
-    (void)argv;
     if (getuid() != 0)
         exit_error("ft_malcolm: Operation not permitted");
     if (argc != 5)
         exit_error("ft_malcolm: usage: <IP-src> <MAC-src> <IP-target> <MAC-target>");
+    signal(SIGINT, signal_handler);
+
+    init_sockaddr(argv[IP_TARGET]);
+    t_arp_hdr packet = create_arp_packet(argv);
+    print_packet(packet);
+    //print_debug();
     return (EXIT_SUCCESS);
 }
